d6da38b81754be3383988e6c7742eed858e68960
Aneesh Sharma <aneeshsharma0671@gmail.com>
2021-11-01 18:25:30 +0530

    Added new UI

---
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    public TMP_Text NPCName;
+    public TMP_Text dialogueText;
+    public Animator anim;
+
+    public Queue<string> sentences;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting Convo with" + dialogue.name);
+
+        anim.SetBool("Is_Open", true);
+
+        NPCName.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        String sentence = sentences.Dequeue();
+
+        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End Convo");
+        anim.SetBool("Is_Open", false);
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,60 +8,19 @@ public class Movement : MonoBehaviour
 {
     public CharacterController2D controller;
     private float horizontalMove;
-    private Vector3 position;
-    private float width;
-    private float height;
     public float Speed;
     public float move;
     public bool jump;
 
-    public void Start()
-    {
-    }
     private void Awake()
     {
-        width = (float)Screen.width / 2.0f;
-        height = (float)Screen.height / 2.0f;
-
     }
     void Update()
-    {
-    /*
-        if(Input.GetKey("a"))
-        {
-            horizontalMove = 1f*Speed;
-        }
-        if (Input.GetKey("d"))
-        {
-            horizontalMove = -1f * Speed;
-        }
-
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-                Vector2 pos = touch.position;
-                pos.x = (pos.x - width) / width;
-                pos.y = (pos.y - height) / height;
-                position = new Vector3(pos.x, pos.y, 0.0f);
-                if (position.x > 0.33f)
-                {
-                    horizontalMove = 1f * Speed;
-                }
-                if (position.x < -0.33f)
-                {
-                    horizontalMove = -1f * Speed;
-                }
-                if (position.x < 0.33f && position.x > -0.33f)
-                {
-                  
-                }
-            
-        }*/
-    
+    {    
     }
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false,jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime,false,jump);
         jump = false;
        // horizontalMove = 0f;
     }
